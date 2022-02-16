125a3d539a49ec691f527e9fcbfe77e36a7ca264
David Luan Gomes <davidluangomeslima@gmail.com>
2022-02-15 21:38:12 -0300

    Inclusao de JS no projeto guiado 1 Iniciado o desenvolvimento do JS para o projeto guiado 1 que foi iniciado com HTML e CSS. Foi desenvolvido inicialmente uma variavel 'let' contendo um 'Array' onde foi inserido os dados como 'nome, telefone e experiencia'. Depois foi desenvolvida a tabela no JS e atribuida a mesma no HTML.

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+//desenvolvimento JS para projeto guiado 1, que teve inicio com HTML e CSS.
+let people = [
+  //váriável com array para inclusão dos dados da tabela.
+  {
+    name: 'David Luan Gomes Lima',
+    tel: '+55 (85) 91234-5678',
+    xp: false
+  },
+  {
+    name: 'Dávina Saldanha de Vasconcelos Gomes',
+    tel: '+55 (85) 99999-9999',
+    xp: true
+  },
+  {
+    name: 'Rosângela Barbosa Gomes',
+    tel: '+55 (85) 98888-8888',
+    xp: true
+  },
+  {
+    name: 'Thalita de Almeida Klippel',
+    tel: '+55 (85) 96666-6666',
+    xp: true
+  },
+  {
+    name: 'Andressa Carneiro de Meneses',
+    tel: '+55 (85) 93333-3333',
+    xp: false
+  }
+]
+
+for (person in people) {
+  //desenvolvimento da tabela e inserção da mesma no HTML.
+  document.querySelector('table.list tbody').innerHTML += `<tr>
+  <td>${people[person].name}</td>
+  <td>${people[person].tel}</td>
+  <td>${people[person].xp ? 'Sim' : 'Não'}</td>
+  <td>
+    <button>Alterar</button>
+  </td>`
+}
